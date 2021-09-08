133a77ac005b4728ba83dd986ab7027d1436a39d
Daryll M <djtm97@gmail.com>
2021-09-07 23:45:19 -0400

    add logic when user does not choose any criteria

--- script.js
+++ script.js
@@ -20,28 +20,34 @@ var getCharTypes = function (charType) {
 // Function to add letters to password (lowercase and uppercase)
 var addLetter = function (pwd, small) {
   const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var index = Math.floor(Math.random() * 27);
+  var index = Math.floor(Math.random() * 26);
+  console.log(index);
   if (small) {
     pwd = pwd + letters[index];
   } else {
     pwd = pwd + letters[index].toUpperCase();
   }
+  console.log(pwd);
   return pwd;
 }
 
 // Function to add a number to passworwd
 var addNumber = function (pwd) {
   const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var index = Math.floor(Math.random() * 11);
+  var index = Math.floor(Math.random() * 10);
+  console.log(index);
   pwd = pwd + nums[index];
+  console.log(pwd);
   return pwd;
 }
 
 // Function to add special characters to passworwd
 var addSpecialChar = function (pwd) {
   const chars = ['!', '@', '#', '$', '%', '^', '&', '*'];
-  var index = Math.floor(Math.random() * 9);
+  var index = Math.floor(Math.random() * 8);
+  console.log(index);
   pwd = pwd + chars[index];
+  console.log(pwd);
   return pwd;
 }
 
@@ -61,11 +67,16 @@ var generatePassword = function () {
   console.log(criteria.length);
 
   // Get character types to put in password
-  criteria.characters[0] = getCharTypes('lowercase characters');
-  criteria.characters[1] = getCharTypes('upercase characters');
-  criteria.characters[2] = getCharTypes('numbers');
-  criteria.characters[3] = getCharTypes('special characters');
-  console.log(criteria.characters);
+  while(criteria.characters[0] === false && criteria.characters[1] === false && criteria.characters[2] === false && criteria.characters[3] === false){
+    criteria.characters[0] = getCharTypes('lowercase characters');
+    criteria.characters[1] = getCharTypes('uppercase characters');
+    criteria.characters[2] = getCharTypes('numbers');
+    criteria.characters[3] = getCharTypes('special characters');
+    console.log(criteria.characters);
+    if(criteria.characters[0] === false && criteria.characters[1] === false && criteria.characters[2] === false && criteria.characters[3] === false){
+      window.alert("You did not select any of the criteria!");
+    }
+  }
 
   // Begin creating password
   for (var i = 0; i < criteria.length; i++) {
