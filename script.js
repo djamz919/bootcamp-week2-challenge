f8455ca3ceb958679ccc7627b8c3d3af69aea61c
Daryll M <djtm97@gmail.com>
2021-09-07 23:11:49 -0400

    create mvp

--- script.js
+++ script.js
@@ -1,4 +1,98 @@
-// Assignment code here
+// Function to ask user to input password length
+var getLength = function (length) {
+  // !!!NEED FUTURE LOGIC FOR NOT ACCEPTING NON-NUMBERS!!!
+  while (length === null || length === "" || length < 8 || length > 128) {
+    length = window.prompt('How many characters would you like in the password?');
+    if (length === null || length === "" || length < 8 || length > 128) {
+      window.alert('Please enter a value at least 8 and no more than 128');
+    }
+  }
+  return length;
+}
+
+// Funciton to ask user what type of character types to include in the password
+var getCharTypes = function (charType) {
+  return (window.confirm('Would you like to use ' + charType + '?'));
+}
+
+// Function to add letters to password (lowercase and uppercase)
+var addLetter = function (pwd, small) {
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var index = Math.floor(Math.random() * 27);
+  if (small) {
+    pwd = pwd + letters[index];
+  } else {
+    pwd = pwd + letters[index].toUpperCase();
+  }
+  return pwd;
+}
+
+// Function to add a number to passworwd
+var addNumber = function (pwd) {
+  const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var index = Math.floor(Math.random() * 11);
+  pwd = pwd + nums[index];
+  return pwd;
+}
+
+// Function to add special characters to passworwd
+var addSpecialChar = function (pwd) {
+  const chars = ['!', '@', '#', '$', '%', '^', '&', '*'];
+  var index = Math.floor(Math.random() * 9);
+  pwd = pwd + chars[index];
+  return pwd;
+}
+
+// Main function for generating password
+var generatePassword = function () {
+  // Create object to hold all criteria variables
+  var criteria = {
+    length: 0,
+    characters: [
+      false, false, false, false
+    ]
+  }
+  var finalPwd = '';
+
+  // Get and set password length
+  criteria.length = getLength(criteria.length);
+  console.log(criteria.length);
+
+  // Get character types to put in password
+  criteria.characters[0] = getCharTypes('lowercase characters');
+  criteria.characters[1] = getCharTypes('upercase characters');
+  criteria.characters[2] = getCharTypes('numbers');
+  criteria.characters[3] = getCharTypes('special characters');
+  console.log(criteria.characters);
+
+  // Begin creating password
+  for (var i = 0; i < criteria.length; i++) {
+    var type = false;
+    var index = -1;
+    while (type === false) {
+      index = Math.floor(Math.random() * 4);
+      type = criteria.characters[index];
+    }
+    console.log(index);
+    // Use switch case to add a character 
+    switch (index) {
+      case 0:
+        finalPwd = addLetter(finalPwd, true); //lowercase
+        break;
+      case 1:
+        finalPwd = addLetter(finalPwd, false); //uppercase
+        break;
+      case 2:
+        finalPwd = addNumber(finalPwd); //numer
+        break;
+      case 3:
+        finalPwd = addSpecialChar(finalPwd); //special characters
+    }
+  }
+
+  return finalPwd;
+}
+
 
 
 // Get references to the #generate element
